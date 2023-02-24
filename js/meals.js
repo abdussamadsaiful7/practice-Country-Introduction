9478b2b52bd8932ed95d20af89ac5353ac66b57b
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-24 19:52:45 +0600

    added modal

--- js/meals.js
+++ js/meals.js
@@ -23,6 +23,10 @@ const displayMeals = meals =>{
                 <div class="card-body">
                    <h5 class="card-title">${meal.strMeal}</h5>
                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                   
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+                        Details
+                    </button>
                 </div>
             </div>
          `;
