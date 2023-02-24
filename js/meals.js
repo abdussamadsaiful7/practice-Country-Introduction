51adcf5c33e15789dc8fd7099fc1d12e277682d4
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-24 20:17:37 +0600

    added js code

--- js/meals.js
+++ js/meals.js
@@ -24,7 +24,7 @@ const displayMeals = meals =>{
                    <h5 class="card-title">${meal.strMeal}</h5>
                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                    
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+                    <button onclick="loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
                         Details
                     </button>
                 </div>
@@ -41,9 +41,22 @@ const searchMeal = () =>{
     const searchText =document.getElementById('search-field').value;
 
     loadMeals(searchText);
-
-    
-    
+ 
 }
 
+const loadMealDetail = idMeal =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetails(data.meals[0]))
+    console.log(idMeal)
+}
+
+const displayMealDetails =meal =>{
+    document.getElementById('mealDetailsLabel').innerText = meal.strMeal;
+    const mealsDetails = document.getElementById('mealDetailsBody');
+    mealsDetails.innerHTML = `
+        <img class="img-fluid" src="${meal.strMealThumb}">
+    `
+}
 loadMeals('fish');
