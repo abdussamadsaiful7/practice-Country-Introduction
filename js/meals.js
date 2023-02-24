792acd90536226ea8a02d1b495201f866f8ebc42
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-24 18:32:18 +0600

    added js code and edit

--- js/meals.js
+++ js/meals.js
@@ -8,6 +8,28 @@ const loadMeals = () =>{
 const displayMeals = meals =>{
    // console.log(meals);
    // step 1: container element,
+   const mealsContainer = document.getElementById('meals-container');
+    meals.forEach(meal =>{
+        console.log(meal)
+        //step-2: create child for each element,
+        const mealDiv = document.createElement('div');
+        mealDiv.classList.add('col');
+        //step-3 set content of the child
+       mealDiv.innerHTML =`
+       <div class="card">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+        </div>
+       `;
+        //step-4 appendChild
+        mealsContainer.appendChild(mealDiv);
+
+
+    })
 }
 
 
