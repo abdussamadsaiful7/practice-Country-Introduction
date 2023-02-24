cd898a2bd3b88042b7ed21b43084e191dad2a06f
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-24 19:42:15 +0600

    added js code

--- js/meals.js
+++ js/meals.js
@@ -1,5 +1,6 @@
-const loadMeals = () =>{
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=fish')
+const loadMeals = (searchText) =>{
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    fetch(url)
     .then(res => res.json())
     .then(data => displayMeals(data.meals));
 
@@ -9,6 +10,7 @@ const displayMeals = meals =>{
    // console.log(meals);
    // step 1: container element,
    const mealsContainer = document.getElementById('meals-container');
+   mealsContainer.innerHTML = '';
     meals.forEach(meal =>{
         console.log(meal)
         //step-2: create child for each element,
@@ -16,21 +18,28 @@ const displayMeals = meals =>{
         mealDiv.classList.add('col');
         //step-3 set content of the child
        mealDiv.innerHTML =`
-       <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <div class="card h-100">
+                <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                   <h5 class="card-title">${meal.strMeal}</h5>
+                   <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
             </div>
-        </div>
-       `;
+         `;
         //step-4 appendChild
         mealsContainer.appendChild(mealDiv);
 
 
-    })
+    });
 }
 
+const searchMeal = () =>{
+    const searchText =document.getElementById('search-field').value;
 
-loadMeals();
+    loadMeals(searchText);
+
+    
+    
+}
+
+loadMeals('fish');
